4bd7a5cd05366c617f17f2b2ecdbba91c5c17589
John Ulbright <john.ulbright@gmail.com>
2021-02-04 21:06:23 -0500

    updated pokemon.js

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,47 +1,47 @@
-import {useEffect,useState} from 'react';
+// import {useEffect,useState} from 'react';
 
 
-const Pokemon=({match})=>{
-    const url=`https://pokeapi.co/api/v2/pokemon/${match.params.name}`;
-const [data, setData]=useState();
-const initData=async()=>{
-    const response = await fetch(url);
-    const pokemon = await response.json();
+// const Pokemon=({match})=>{
+//     const url=`https://pokeapi.co/api/v2/pokemon/${match.params.name}`;
+// const [data, setData]=useState();
+// const initData=async()=>{
+//     const response = await fetch(url);
+//     const pokemon = await response.json();
 
-    setData(pokemon);
-};
-    useEffect(()=>{
-        initData();
-    },[])
-return(
-        <div>
-            <h1>Pokemon</h1>
-        </div>
-    )
-}
-
-export default Pokemon;
-
-// const Pokemon=(props)=>{
-//     const goHome=()=>{
-//         props.history.push('/');
-//     }
-//     const new_url=`https://pokeapi.co/api/v2/pokemon/${props.match.params.name}`;
-//     let img_url="";
-//     fetch(new_url)
-//     .then(res=>res.json())
-//     .then(json=>{
-//         img_url=json.sprites.front_default;
-//         console.log(img_url)
-//     })
-//     return(
+//     setData(pokemon);
+// };
+//     useEffect(()=>{
+//         initData();
+//     },[])
+// return(
 //         <div>
-//             <button onClick={goHome}>Go home</button>
 //             <h1>Pokemon</h1>
-//             <p>{props.match.params.name}</p>
-//             <img src={`${img_url}`}/>
 //         </div>
 //     )
 // }
 
 // export default Pokemon;
+import {useEffect,useState} from 'react';
+
+const Pokemon=(props)=>{
+    const [imgUrl,setImgUrl]=useState("");
+    const goHome=()=>{
+        props.history.push('/');
+    }
+    const new_url=`https://pokeapi.co/api/v2/pokemon/${props.match.params.name}`;
+    fetch(new_url)
+    .then(res=>res.json())
+    .then(json=>{
+        setImgUrl(json.sprites.front_default);
+    })
+    return(
+        <div>
+            <button onClick={goHome}>Go home</button>
+            <h1>Pokemon</h1>
+            <p>{props.match.params.name}</p>
+            <img src={imgUrl}/>
+        </div>
+    )
+}
+
+export default Pokemon;
